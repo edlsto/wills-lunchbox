b90a22848f3da84a134f5e6772d968bd98cb68bc
Edward Stoner <edwardlstoner@gmail.com>
2019-12-22 12:52:57 -0700

    Pass tests

--- snack.js
+++ snack.js
@@ -1,5 +1,16 @@
 class Snack {
+  constructor(type) {
+    this.deliciousLevel = 'extra';
+    this.type = type;
+    this.amount = 100;
+    this.isInLunchBox = false;
+    this.healthy = type.includes('Fruit') || false;
+  }
 
+  getEaten() {
+    this.amount -= 10;
+    this.cuttingItClose = (this.amount / 100) <= .2 || false;
+  }
 }
 
 module.exports = Snack;
