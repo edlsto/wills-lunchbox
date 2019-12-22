bf323721e1b757952ad83fb9a5d7236b9b5d5d99
Edward Stoner <edwardlstoner@gmail.com>
2019-12-22 12:52:48 -0700

    Pass tests

--- lunchbox.js
+++ lunchbox.js
@@ -1,5 +1,28 @@
-class LunchBox {
 
+class LunchBox {
+  constructor(lunchboxDetails) {
+    this.owner = lunchboxDetails.owner;
+    this.material = lunchboxDetails.madeOf;
+    this.shape = lunchboxDetails.shape;
+    this.color = lunchboxDetails.color;
+    this.handle = true;
+    this.snacks = [];
+  }
+
+  acquireSnack(fruitSnack) {
+    this.snacks.push(fruitSnack);
+    fruitSnack.isInLunchBox = true;
+  }
+
+  findHealthySnacks() {
+    var healthySnacksList = [];
+    for (var i = 0; i < this.snacks.length; i++) {
+      if (this.snacks[i].healthy) {
+        healthySnacksList.push(this.snacks[i])
+      }
+    }
+    return healthySnacksList;
+  }
 }
 
 module.exports = LunchBox;
